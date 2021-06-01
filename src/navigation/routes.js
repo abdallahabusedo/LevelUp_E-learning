12b3d9688e450905837f84fc9ca9835c8f133227
abdallahabusedo <abozoooz2014.aa@gmail.com>
2021-06-02 00:04:11 +0200

    🎯 Add the video to the videoPage

--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -5,20 +5,21 @@ import Home from '../pages/Home/home';
 import SignUp from '../pages/AccountLogin/signup';
 import SignIn from '../pages/AccountLogin/signin';
 import Profile from '../pages/User/Profile';
-
+import VideoPage from "../pages/videoPage/videoPage.jsx";
 const createRoutes = () => (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/home" />
+      </Route>
 
-        <Route exact path="/home" component={ Home }/>
-        <Route exact path="/signup" component={ SignUp }/>
-        <Route exact path="/signin" component={ SignIn }/>
-        <Route exact path="/user/profile" component={ Profile } />
-      </Switch>
-    </Router>
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/user/profile" component={Profile} />
+      <Route exact path="/VideoPage" component={VideoPage} />
+    </Switch>
+  </Router>
 );
 
 export default createRoutes;
