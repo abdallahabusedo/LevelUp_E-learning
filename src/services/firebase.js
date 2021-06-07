cb6f23e0adbeb02c8191cde8f05764ae1ff8c244
abdallahabusedo <abozoooz2014.aa@gmail.com>
2021-06-08 01:32:19 +0200

    EDIT : firebase file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,28 +1,29 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/storage';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/firestore";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDojOMJLCVTE8UOuM7ubxjxxPLMF7W2AF0",
-//     authDomain: "levelup-bb9e8.firebaseapp.com",
-//     databaseURL: "https://levelup-bb9e8-default-rtdb.firebaseio.com",
-//     projectId: "levelup-bb9e8",
-//     storageBucket: "levelup-bb9e8.appspot.com",
-//     messagingSenderId: "602519641720",
-//     appId: "1:602519641720:web:ab9038e83deffcfd4ed78d",
-//     measurementId: "G-1N5DR43B1T"
-// };
-
+require("dotenv").config();
+//console.log(process.env.API_KEY);
 const firebaseConfig = {
-    apiKey: "AIzaSyBUxJBKjNK5SYPRH0pO8OT3jBkf1dVhfyQ",
-    authDomain: "lvlup-e2894.firebaseapp.com",
-    projectId: "lvlup-e2894",
-    storageBucket: "lvlup-e2894.appspot.com",
-    messagingSenderId: "1033867326442",
-    appId: "1:1033867326442:web:3bef29fb112bea234e3893",
-    measurementId: "G-1CP8NMGERN"
-  };
+  apiKey: "AIzaSyDobuBTPeK2TQ9kTuU0NJ4EZYS2VSqkYCk",
+  authDomain: "lvlup2.firebaseapp.com",
+  projectId: "lvlup2",
+  storageBucket: "lvlup2.appspot.com",
+  messagingSenderId: "812541653360",
+  appId: "1:812541653360:web:7158d51446ab0f69911fb5",
+  measurementId: "G-Z41X0QQXFB",
+};
+
+/*
+apiKey: "AIzaSyDobuBTPeK2TQ9kTuU0NJ4EZYS2VSqkYCk",
+    authDomain: "lvlup2.firebaseapp.com",
+    projectId: "lvlup2",
+    storageBucket: "lvlup2.appspot.com",
+    messagingSenderId: "812541653360",
+    appId: "1:812541653360:web:7158d51446ab0f69911fb5",
+    measurementId: "G-Z41X0QQXFB"
+*/
 
 firebase.initializeApp(firebaseConfig);
 
