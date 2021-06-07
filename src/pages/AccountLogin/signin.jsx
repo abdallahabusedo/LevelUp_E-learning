bdd403c77a1667ff5fb41d0f0841439a48650fb3
abdallahabusedo <abozoooz2014.aa@gmail.com>
2021-06-08 01:31:46 +0200

    FIX : center div

--- src/pages/AccountLogin/signin.jsx
+++ src/pages/AccountLogin/signin.jsx
@@ -1,100 +1,129 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import NavigationBar from "../../components/navComponent";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import { fireStore } from "../../services/firebase";
 import { GoogleAuth, userAuth } from "../../services/Authentication";
 
 import "../../assets/styles/Form.css";
 export default class SignIn extends Component {
-
-  constructor(props)
-  {
-      super(props);
-      this.state = {
-          email:'',
-          password:'',
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
   }
 
-  handleChange = ( event ) => {
-      let name = event.target.name , value = event.target.value;
-      this.setState({ [name]:value } );
-  }
+  handleChange = (event) => {
+    let name = event.target.name,
+      value = event.target.value;
+    this.setState({ [name]: value });
+  };
 
-  handleSubmit = ( event ) => {
-      event.preventDefault();
-      
-      let {email , password} = this.state;
-      if( event.target.name === "userAuth") {
-          userAuth( email , password ).then( () => {
-              console.log("User Auth Success");
-              this.props.history.push('/user/profile');
-          })
-          .catch(err => {
-              alert(err.message);
-          });
-      } else if( event.target.name === "googleAuth") {
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-          GoogleAuth().then( (result) => {
-              console.log("Google Auth Success");
-              var ref = fireStore.collection("users");
-              
-              ref.get(result.user.uid).then((doc) => {
-                if (!doc.exists) {
-                  ref.doc(result.user.uid).set({
-                      username: result.user.displayName,
-                      profileImage: "",
-                      job: "",
-                      email: result.user.email,
-                      password: "Google Sign In",
-                      Bio: "",
-                      LinkGitHub: "",
-                      LinkLinkedIn: "",
-                  }).then( () => {
-                    this.props.history.push('/user/profile');
-                    //this.props.history.push("/");
-                  });
-                } else {
-                  this.props.history.push('/user/profile');
+    let { email, password } = this.state;
+    if (event.target.name === "userAuth") {
+      userAuth(email, password)
+        .then(() => {
+          console.log("User Auth Success");
+          this.props.history.push("/user/profile");
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    } else if (event.target.name === "googleAuth") {
+      GoogleAuth()
+        .then((result) => {
+          console.log("Google Auth Success");
+          var ref = fireStore.collection("users");
+
+          ref.get(result.user.uid).then((doc) => {
+            if (!doc.exists) {
+              ref
+                .doc(result.user.uid)
+                .set({
+                  username: result.user.displayName,
+                  profileImage: "",
+                  job: "",
+                  email: result.user.email,
+                  password: "Google Sign In",
+                  Bio: "",
+                  LinkGitHub: "",
+                  LinkLinkedIn: "",
+                })
+                .then(() => {
+                  this.props.history.push("/user/profile");
                   //this.props.history.push("/");
-                }
-              });
-                
-          }).catch ( err => {
-              console.log( err.message );
+                });
+            } else {
+              this.props.history.push("/user/profile");
+              //this.props.history.push("/");
+            }
           });
-      } 
-  } 
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  };
 
   render() {
     return (
       <div className="form-container">
-        <NavigationBar/>
+        <NavigationBar />
         <h1> Login </h1>
-        <hr/>
+        <hr />
         <form name="userAuth" onSubmit={this.handleSubmit}>
           <div className="form-group" onClick={this.handleSubmit}>
-            <button type="button" name="googleAuth" > <img src="https://www.iconfinder.com/data/icons/social-media-2210/24/Google-512.png" alt="" name="googleAuth"/> Continue with Google </button>
+            <button type="button" name="googleAuth">
+              {" "}
+              <img
+                src="https://www.iconfinder.com/data/icons/social-media-2210/24/Google-512.png"
+                alt=""
+                name="googleAuth"
+              />{" "}
+              Continue with Google{" "}
+            </button>
           </div>
           <div className="form-group">
             <label>Email address</label>
-            <input type="email" required="" className="form-control" id="email" placeholder="Email" name="email" onChange={this.handleChange}/>
+            <input
+              type="email"
+              required=""
+              className="form-control"
+              id="email"
+              placeholder="Email"
+              name="email"
+              onChange={this.handleChange}
+            />
           </div>
           <div className="form-group">
             <label>Password</label>
-            <input type="password" required="" className="form-control" id="password" placeholder="Password" name="password" onChange={this.handleChange}/>
+            <input
+              type="password"
+              required=""
+              className="form-control"
+              id="password"
+              placeholder="Password"
+              name="password"
+              onChange={this.handleChange}
+            />
           </div>
           <div className="form-actions">
             <input type="submit" value="Sign In" />
-            <span>or</span><Link to="/"> forgot password</Link>
+            <span>or</span>
+            <Link to="/"> forgot password</Link>
           </div>
           <div className="form-footer">
-            <span>Don't have an account </span><Link to="/signup">Register</Link>
+            <span>Don't have an account </span>
+            <Link to="/signup">Register</Link>
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,58 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import NavigationBar from "../../components/navComponent";
-import "../../assets/styles/Search.css"
+import "../../assets/styles/Search.css";
 
-import 'firebase/firestore';
+import "firebase/firestore";
 
-import firebase from "../../services/firebase"
+import firebase from "../../services/firebase";
 import { useParams } from "react-router-dom";
 
-import Card from "../../components/Card"
+import Card from "../../components/Card";
 export default function Search() {
-    
-    let { id } = useParams();
-  
-    const [results, setResults] = useState([])
-    const [loading, setloading] = useState(true)
-    const ref = firebase.firestore().collection("courses").where('keywords', 'array-contains', id);
-    function getSearchResults() {
+  let { id } = useParams();
 
-        ref.get().then((Snapshot) => {
-            setloading(false)
-            if (Snapshot.empty) {
+  const [results, setResults] = useState([]);
+  const [loading, setloading] = useState(true);
+  const ref = firebase
+    .firestore()
+    .collection("courses")
+    .where("keywords", "array-contains", id);
+  function getSearchResults() {
+    ref.get().then((Snapshot) => {
+      setloading(false);
+      if (Snapshot.empty) {
+        return;
+      }
+      const items = [];
+      Snapshot.forEach((doc) => {
+        items.push(doc);
+      });
+      setResults(items);
+    });
+  }
+  useEffect(() => {
+    getSearchResults();
+  }, []);
+  let cards = [];
 
-                return;
-            }
-            const items = [];
-            Snapshot.forEach(doc => {
-                items.push(doc);
-
-            });
-            setResults(items);
-        });
-    }
-    useEffect(() => {
-
-        getSearchResults();
- 
-    },[]);
-    let cards = [];
-
-    results.map((e,index) => cards.push(<Card courseInfo={e.data()} id={e.id} key={index}></Card>));
-    if (loading) {
-        return (<div>loading</div>);
-    }
-    return (
-
-        <div>
-            <header className="page-header" >
-                <NavigationBar />
-            </header>
-            <h1>search results</h1>
-            <br></br>
-            <div className="results">
-                {cards}
-            </div >
-        </div >
-    );
+  results.map((e, index) =>
+    cards.push(<Card courseInfo={e.data()} id={e.id} key={index}></Card>)
+  );
+  if (loading) {
+    return <div>loading</div>;
+  }
+  return (
+    <div>
+      <header className="page-header">
+        <NavigationBar />
+      </header>
+      <h1>search results</h1>
+      <br></br>
+      <div className="results">{cards}</div>
+    </div>
+  );
 }
