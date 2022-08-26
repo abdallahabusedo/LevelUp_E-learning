1a73cbc3e54ffbb571f9cbe6ade825556d9134be
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2022-08-26 22:36:25 +0200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,27 +4,6 @@ import "firebase/storage";
 import "firebase/firestore";
 
 require("dotenv").config();
-//console.log(process.env.API_KEY);
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDobuBTPeK2TQ9kTuU0NJ4EZYS2VSqkYCk",
-  authDomain: "lvlup2.firebaseapp.com",
-  projectId: "lvlup2",
-  storageBucket: "lvlup2.appspot.com",
-  messagingSenderId: "812541653360",
-  appId: "1:812541653360:web:7158d51446ab0f69911fb5",
-  measurementId: "G-Z41X0QQXFB",
-};*/
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDobuBTPeK2TQ9kTuU0NJ4EZYS2VSqkYCk",
-  authDomain: "lvlup2.firebaseapp.com",
-  projectId: "lvlup2",
-  storageBucket: "lvlup2.appspot.com",
-  messagingSenderId: "812541653360",
-  appId: "1:812541653360:web:7158d51446ab0f69911fb5",
-  measurementId: "G-Z41X0QQXFB"
-};
 
 firebase.initializeApp(firebaseConfig);
 
